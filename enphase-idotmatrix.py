f4d199e0051cc2614d5df06f32419401b64d2d67
AlecIsaacson <alec.isaacson@grafana.com>
2026-06-18 20:17:57 -0400

    handle zeros

--- enphase-idotmatrix.py
+++ enphase-idotmatrix.py
@@ -10,7 +10,7 @@ import logging
 import datetime
 
 logging.Formatter.converter = time.gmtime
-logging.basicConfig(filename='/home/alec/GitHub/enphase-idotmatrix/enphase-idotmatrix.log', level=logging.INFO, format='%(asctime)s.%(msecs)03dZ %(levelname)s : %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+logging.basicConfig(filename='/home/alec/GitHub/enphase-idotmatrix/enphase-idotmatrix.log', level=logging.DEBUG, format='%(asctime)s.%(msecs)03dZ %(levelname)s : %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
 logger = logging.getLogger(__name__)
 
 logger.info('Starting')
@@ -34,7 +34,17 @@ def getGraphData(queryEndpoint, metric, startTime, endTime):
    except Exception as err:
       logger.info(str(err))
 
-def generateGraph(graphProdData, graphConsumeData, graphDailyProdData, minuteDots):
+def combineDicts(list1, list2):
+   logger.debug('In combineDicts')
+   try:
+      dict1 = dict(list1)
+      dict2 = dict(list2)
+      combinedDict = dict(sorted(dict1.items() | dict2.items()))
+      return(combinedDict)
+   except Exception as err:
+      logger.info(str(err))
+
+def generateGraph(prodDict, consumeDict, dailyProdData, minuteDots):
     logger.debug('In generateGraph')
     X=0
     image = Image.new("RGB", (64,64), (0,0,0))
@@ -49,9 +59,9 @@ def generateGraph(graphProdData, graphConsumeData, graphDailyProdData, minuteDot
     draw.line([(63,0),(63,int(minuteDots))], fill=GREY, width=1)
 
     logger.debug('Processing production data')
-    for prodWatts in graphProdData:
-      pixels = int(float(prodWatts[1])) // 240
-      logger.debug(str(int(float(prodWatts[1]))) + ' ' + str(pixels) + ' ' + str(X))
+    for prodWatts in prodDict.values():
+      pixels = int(float(prodWatts)) // 240
+      logger.debug(str(int(float(prodWatts))) + ' ' + str(pixels) + ' ' + str(X))
       if pixels > 0:
          logger.debug('Line: ' + str(X) + ',37 -> ' + str(X) + ',' + str(37 - pixels))
          draw.line([(X,37),(X,37 - (pixels - 1))], fill=GREEN, width=1)
@@ -59,22 +69,22 @@ def generateGraph(graphProdData, graphConsumeData, graphDailyProdData, minuteDot
 
     logger.debug('Processing consumption data')
     X=0
-    for consumeWatts in graphConsumeData:
-      pixels = int(float(consumeWatts[1])) // 240
-      logger.debug(str(int(float(consumeWatts[1]))) + ' ' + str(pixels) + ' ' + str(X))
+    for consumeWatts in consumeDict.values():
+      pixels = int(float(consumeWatts)) // 240
+      logger.debug(str(int(float(consumeWatts))) + ' ' + str(pixels) + ' ' + str(X))
       if pixels > 0:
          logger.debug('Line: ' + str(X) + ',39 -> ' + str(X) + ',' +str(39 + pixels))
          draw.line([(X,39),(X,39 + (pixels - 1))], fill=RED, width=1)
       X+=1
-
-    currentProd = float(graphProdData[-1][1]) - float(graphConsumeData[-1][1])
+    
+    currentProd = float(next(reversed(prodDict.values()))) - float(next(reversed(consumeDict.values())))
     logger.debug(str(currentProd))
     if currentProd < 0:
       fillColor = RED
     else:
       fillColor = GREEN
     draw.text((1,1), str(int(currentProd)), font=font, fill=fillColor)
-    draw.text((32,1), str(round(float(graphDailyProdData), 2)), font=font, fill=GREEN, anchor='mt')
+    draw.text((32,1), str(round(float(dailyProdData), 2)), font=font, fill=GREEN, anchor='mt')
 
     image.save('/tmp/combined.png')
 
@@ -91,16 +101,15 @@ def main():
    startTime = endTime - (60 * 960) 
    logger.info('End: ' + str(endTime) + ' ' + 'Start: ' + ' ' + str(startTime))
 
-   graphProdResponse = getGraphData('/api/datasources/proxy/uid/grafanacloud-prom/api/v1/query_range', 'solar_prod_wnow', startTime, endTime)
-   graphConsumeResponse = getGraphData('/api/datasources/proxy/uid/grafanacloud-prom/api/v1/query_range', 'solar_consume_wnow', startTime, endTime)
-   logger.debug('Graph prod response:' + ' ' + str(graphProdResponse)) 
-   logger.debug('Graph consume response:' + ' ' + str(graphConsumeResponse))
+   graphProdResponse = getGraphData('/api/datasources/proxy/uid/grafanacloud-prom/api/v1/query_range', 'solar_prod_wnow or on() vector(0)', startTime, endTime)
+   prodDict = combineDicts(graphProdResponse['data']['result'][0]['values'], graphProdResponse['data']['result'][1]['values'])
 
-   graphDailyProdResponse = getGraphData('/api/datasources/proxy/uid/grafanacloud-prom/api/v1/query', 'last_over_time(solar_prod_whtoday[16h]) / 1000', startTime, endTime)
-   logger.debug('Daily prod response:' + ' ' + str(graphDailyProdResponse))
+   graphConsumeResponse = getGraphData('/api/datasources/proxy/uid/grafanacloud-prom/api/v1/query_range', 'solar_consume_wnow or on() vector(0)', startTime, endTime)
+   consumeDict = combineDicts(graphConsumeResponse['data']['result'][0]['values'], graphConsumeResponse['data']['result'][1]['values'])
 
-   generateGraph(graphProdResponse['data']['result'][0]['values'], graphConsumeResponse['data']['result'][0]['values'], graphDailyProdResponse['data']['result'][0]['value'][1], minuteDots)
+   dailyProdResponse = getGraphData('/api/datasources/proxy/uid/grafanacloud-prom/api/v1/query', 'last_over_time(solar_prod_whtoday[16h]) / 1000', startTime, endTime)
 
+   generateGraph(prodDict, consumeDict, dailyProdResponse['data']['result'][0]['value'][1], minuteDots)
 
 if __name__ == '__main__':
    main()
